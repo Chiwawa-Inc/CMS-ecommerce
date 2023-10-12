a98d73f9354b0f016b22298b98c9637900f51604
sanil shakya <np03cs4a220052.heraldcollege.edu.np>
2023-10-12 12:50:36 +0545

    added products and services to router and nav links in navbar

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,15 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
 
-import App from "../App";
-import Contact from "../pages/contact";
-import Products from "../pages/products";
-import Services from "../pages/services";
+import App from "../App"
+import Home from "../pages/home"
+import Contact from "../pages/contact"
+import Products from "../pages/products"
+import Services from "../pages/services"
 // import
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
             {
                 path: "/products",
                 element: <Products />,
@@ -24,6 +29,6 @@ const router = createBrowserRouter([
             },
         ],
     },
-]);
+])
 
-export default router;
+export default router
