c6bcbbbae8412113968ca2cd30430e80e46affe6
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2023-10-13 21:57:03 +0545

    contact banner

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./components/aboutus";
 import CustomCarousel from "./components/carousel"
+import Contact from "./components/contactbanner";
 
 export default function index() {
     return(
         <>
         <CustomCarousel/>
         <About/>
+        <Contact/>
         </>
     )
     
