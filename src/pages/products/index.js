d836f6652bd8651e124f240830d0a7c08f1594e4
Sanil Shakya <69036430+saw-zer0@users.noreply.github.com>
2023-10-21 23:22:45 +0545

    Products/components (#4)
    * fixed navbar links not working
    added categories components
    some css changes
    
    * added products subcategories and brands filter components
    
    ---------
    
    Co-authored-by: sanil shakya <np03cs4a220052.heraldcollege.edu.np>

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,18 +2,24 @@ import React from "react"
 import CategoriesCard from "./components/CategoriesCard"
 import { Col, Row, Stack } from "react-bootstrap"
 import { Container } from "react-bootstrap"
+import FilterSection from "./components/FilterSection"
 
 export default function Products() {
     return (
         <Container>
-            <Stack direction="horizontal" gap={3}>
+            <Stack
+                direction="horizontal"
+                gap={3}
+                className="mx-auto my-3"
+                style={{ justifyContent: "center" }}
+            >
                 <CategoriesCard />
                 <CategoriesCard />
                 <CategoriesCard />
             </Stack>
             <Row md={2}>
                 <Col md={3} xs={1}>
-                    asdf
+                    <FilterSection />
                 </Col>
                 <Col>asdf</Col>
             </Row>
