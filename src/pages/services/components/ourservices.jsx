8498a0e925030cfe439d81077d61b89d0952e7df
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2023-10-31 13:46:45 +0545

    servives list, cards

---
+++ src/pages/services/components/ourservices.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Service(){
+    return(
+            <div className="" style = {{marginTop: "5em"}}>
+            <h1 className="text-center">Our Services</h1>
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum porro voluptatem, sunt expedita facilis totam quo omnis quasi dolorem modi tempora assumenda accusantium laborum nostrum quos voluptatum quaerat recusandae.</p>
+            </div>
+            
+    )
+}
