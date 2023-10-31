c38f05095dfd8d149340418e3c4e47d329f68bec
Sanil Shakya <69036430+saw-zer0@users.noreply.github.com>
2023-10-31 13:58:44 +0545

    Products/components (#5)
    * fixed navbar links not working
    added categories components
    some css changes
    
    * added products subcategories and brands filter components
    
    * added product card components
    
    * added acordion component, added products card and product list display
    
    ---------
    
    Co-authored-by: sanil shakya <np03cs4a220052.heraldcollege.edu.np>

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,10 +1,28 @@
-import React from "react"
+import React, { useEffect } from "react"
 import CategoriesCard from "./components/CategoriesCard"
 import { Col, Row, Stack } from "react-bootstrap"
 import { Container } from "react-bootstrap"
 import FilterSection from "./components/FilterSection"
+import ProductCard from "./components/ProductCard"
 
 export default function Products() {
+    useEffect(()=>{
+        
+    },[])
+    const products =  [
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+    ]
     return (
         <Container>
             <Stack
@@ -17,11 +35,21 @@ export default function Products() {
                 <CategoriesCard />
                 <CategoriesCard />
             </Stack>
-            <Row md={2}>
-                <Col md={3} xs={1}>
+            <Row>
+                <Col md={3}>
                     <FilterSection />
                 </Col>
-                <Col>asdf</Col>
+                <Col md={9} >
+                <Row xs={2} md={2} lg={3} xl={4}>
+                    {products.map(product => {
+                        return(
+                            <Col style={{margin: "1em 0"}}>
+                                <ProductCard/>
+                            </Col> 
+                        )
+                    })}
+                </Row>
+                </Col>
             </Row>
         </Container>
     )
