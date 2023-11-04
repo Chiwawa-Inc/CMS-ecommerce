683f504b07367279d93f8c086ff32f5e961336df
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2023-11-04 15:03:39 +0545

    required fills in form

--- src/pages/contact/components/form.jsx
+++ src/pages/contact/components/form.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 
 export default function Form(){
     const initialValues = {username: "", email: "", phonenumber: ""};
     const [formValues, setFormValues] = useState(initialValues)
-    const [formErrors, setFormErrors] = useState()
-    
-
+    const [formErrors, setFormErrors] = useState({})
+    const [isSubmit, setIsSubmit] = useState(false)
 
     const handleChange = (e) => {
         console.log(e.target)
         const{name, value} = e.target
         setFormValues({ ...formValues, [name]: value})
         console.log(formValues)
+        setIsSubmit(true)
     }
 
     const handleSubmit = (e) => {
@@ -20,6 +20,12 @@ export default function Form(){
         setFormErrors(validate(formValues))
     }
 
+    useEffect(() =>{
+        console.log(formErrors)
+        if(Object.keys(formErrors).length === 0 && isSubmit){
+            console.log(formValues)
+        }
+    }, [formErrors])
     const validate = (values) => {
         const errors = {}
         const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i
@@ -28,13 +34,14 @@ export default function Form(){
     
         }
         if (!values.email){
-        errors.username = "email is required"
+        errors.email = "Email is required"
 
     }
     if (!values.phonenumber){
-    errors.username = "Phone number is required"
+    errors.phonenumber = "Phone number is required"
     }
-    return
+    return errors;
+
 }
     return(
         <div>
@@ -44,14 +51,19 @@ export default function Form(){
                 <label>Username</label>
                 <input type="text" name = "username" placeholder= "Username" value = {formValues.username} onChange = {handleChange} ></input>
                 </div>
+                <p>{formErrors.username}</p>
                 <div className="field">
                     <label>Email Address</label>
                     <input type = "email" name = "email" placeholder = "Email" value = {formValues.email} onChange = {handleChange} />
                 </div>
+                <p>{formErrors.email}</p>
+
                 <div className="field">
                     <label>Phone Number</label>
-                    <input type = "number" name = "phonenumber" placeholder = "Number" value = {formValues.phonenumber} onChange = {handleChange} />
+                    <input type = "string" name = "phonenumber" placeholder = "Number" value = {formValues.phonenumber} onChange = {handleChange} />
                 </div>
+                <p>{formErrors.phonenumber}</p>
+
                 <button>Submit</button>
             </form>
         </div>
