1c8b9b30866c52983572408f74f53ce489069256
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2023-11-26 18:29:15 +0545

    services types

---
+++ src/pages/services/components/title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function Mep(){
+    return(
+        <>
+        <Container>
+            <h1>MEP Contracting Services</h1>
+            <div>Welcome to Automatica Technologies Pvt. Ltd. (Nepal), where we offer a dynamic suite of Mechanical, Electrical, and Plumbing (MEP) Services that fuse together cutting-edge technology, sustainable solutions, and unparalleled expertise. With a strong commitment to excellence in electrical, electronics, and information technology services, we are your trusted partner in creating environments that are efficient, eco-conscious, and optimized for the future.
+            </div>
+        </Container>
+        </>
+    )
+}
--- src/pages/services/serviceDetailPage.js
+++ src/pages/services/serviceDetailPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Mep from "./components/title";
+import DiverseServices from "./components/diverseServices";
+import { Container } from "react-bootstrap";
+import ImgOverlayExample from "./components/card";
 
 export default function ServiceDetailPage(){
     return(
-        <div></div>
+        <Container>
+        <Mep/>
+        <DiverseServices/>
+        <ImgOverlayExample/>
+        </Container>
     )
 }
